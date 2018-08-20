5ff91a8be7517b7e1ac15d3e3b537c5520e578f5
luckydarts <yoshiyuumi@yahoo.co.jp>
2018-08-20 10:29:53 +0900

    a

---
+++ CampChallenge_java/015.標準クラス/HC/HClass_1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.HC;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class HClass_1 {
+
+    public static void main(String[]args){
+
+        Calendar c = Calendar.getInstance();
+        
+        c.set(2016,0,1,0,0,0);
+        Date date = c.getTime();
+        
+        System.out.println(date);
+    }
+}
